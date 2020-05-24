b193c6c21a7329809db6479c977c3d546ebb6434
Felipe Silveira <zfelipesilveira@gmail.com>
2020-05-24 05:27:28 -0300

    Criação de 4 classes básicas -- App.java é a que mais precisa ser desenvolvida

---
+++ App.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App {
+
+    public static void main(String[] args) {
+        String linhas[] = new String[1000];
+        int numLinhas = 0;
+        int numPaginas = 1;
+
+        Path path1 = Paths.get("teste.txt");// java.txt AQUI TEMOS QUE COLOCAR ALGUM DOS TEXTOS QUE ELA DEU
+
+        try (BufferedReader reader = Files.newBufferedReader(path1, Charset.defaultCharset())) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                linhas[numLinhas] = line;
+                numLinhas++;
+                if (numLinhas % 40 == 0) {
+                    numPaginas++;
+                }
+            }
+        } catch (IOException e) {
+            System.err.format("Erro na leitura do arquivo: ", e);
+        }
+
+        int n = 0; // ISSO AQUI ACHO QUE NÃO PRECISA, PQ SÓ IMPRIME O LIVRO
+        for (int i = 1; i <= numPaginas; i++) {
+            System.out.println("--------------------- Pagina " + i + " ---------------------");
+            for (int j = 0; j < 40 && n < numLinhas; j++, n++) {
+                System.out.println(linhas[n]);
+            }
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("\nPalavras da linha " + i + ": ");
+            linhas[i-1] = linhas[i-1].replaceAll("\\t"," "); // substitui tab por espaco em branco
+            linhas[i-1] = linhas[i-1].replaceAll(",",""); // para remover vírgulas
+            String[] tokens = linhas[i-1].split(" "); // divide a string pelo espaco em branco
+            for (String s : tokens) {
+                s = s.toLowerCase();
+                System.out.println("->" + s);// AQUI ACHO QUE TEMOS QUE ADICIONAR O "s" na LinkedListPalavra
+                                            // se ainda não existir uma palavra igual. Se existir uma palavra
+                                            // igual, temos que pegar o número da página e adicionar na LinkedListOfInteger
+                                            // dessa palavra.
+            }
+        }
+    }
+}
---
+++ Palavra.java
@@ -0,0 +1,14 @@
+public class Palavra {
+    private String palavra;
+    private LinkedListOfInteger listapaginas;
+
+    public Palavra(String palavra){
+        this.palavra = palavra;
+    }
+
+    public String getPalavra(){
+        return palavra;
+    }
+
+    
+}
